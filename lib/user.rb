5412ea2c2720c8f20259216db8875b9e0c8b248d
Tymeckia Kendall <tykend@gmail.com>
2019-03-28 00:45:55 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,8 +1,9 @@
 class User
   attr_accessor :first_name, :last_name, :user
   
-  def initialize (first_name, last_name)
+  def initialize (first_name, last_name, user)
     @first_name=first_name
     @last_name=last_name
+    @user=user
   end
 end
